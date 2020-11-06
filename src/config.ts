8a7c2838ab359b70cbde615247726698f2ae46e1
iwazowsky <hvazgen@gmail.com>
2020-11-06 22:43:29 +0400

    changed from dev to staging endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
-export const endpoint = "http://192.168.3.179:8000"
-// export const endpoint = "https://allarmlinks-admin-staging.herokuapp.com"
+// export const endpoint = "http://192.168.3.179:8000"
+export const endpoint = "https://allarmlinks-admin-staging.herokuapp.com"
 
