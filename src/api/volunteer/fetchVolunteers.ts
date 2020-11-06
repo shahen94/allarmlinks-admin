7f9e802bb11ae2d49766a7ac9a6fb914e9769cdd
iwazowsky <hvazgen@gmail.com>
2020-11-06 20:40:36 +0400

    adapted to endpoint response

--- src/api/volunteer/fetchVolunteers.ts
+++ src/api/volunteer/fetchVolunteers.ts
@@ -1,7 +1,21 @@
 import { get } from '../../utils/fetch'
-import { AxiosRequestConfig } from 'axios';
-const fetchVolunteers = async (): Promise<object> => {
-    const records: AxiosRequestConfig = await get('MOCK_DATA.json')
-    return records.data
+import { AxiosRequestConfig } from 'axios'
+import IVolunteersRequest from './../../types/volunteers/IVolunteersRequest'
+import { endpoint } from './../../config'
+import qs from 'qs'
+const fetchVolunteers = async (params: IVolunteersRequest): Promise<object> => {
+    const query = qs.stringify(params)
+    const example = qs.stringify({
+        limit: 10,
+        filter: {
+            name: "Khcho"
+        }
+    })
+    console.log(example)
+    // const url = `${endpoint}/volunteer?${query}`
+    const url = `${endpoint}/volunteer?${example}`
+    const response: AxiosRequestConfig = await get(url)
+    console.log(response.data.data)
+    return response.data.data
 }
 export default fetchVolunteers
