9855d9707b63cb456f02bf12ce06369c6d622b6f
Gayane_1 <gmiranyan1@gmail.com>
2020-11-12 14:12:11 +0400

    Fixed search bar styles

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -4,140 +4,165 @@ import MenuItem from "@material-ui/core/MenuItem";
 import Select from "@material-ui/core/Select";
 import SearchIcon from "@material-ui/icons/Search";
 import Grid from "@material-ui/core/Grid";
-import InputBase from "@material-ui/core/InputBase";
 import Button from "@material-ui/core/Button";
 import { useDispatch } from "react-redux";
 import { searchVolunteers } from "../../store/features/volunteersSlice";
 import { searchAdmins } from "../../store/features/adminsSlice";
+import { FormControl, Input, InputAdornment } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
-    searchBar: {
-        minWidth: "570px",
-    },
-    selectSection: {
+  selectSection: {
+    background: "white",
+    marginRight: "1px",
+    padding: "6px 30px 4px 45px",
+    borderBottomLeftRadius: "20px",
+    borderTopLeftRadius: "20px",
+  },
+  searchSection: {
+    background: "white",
+    marginLeft: "1px",
+    padding: "4px 30px",
+    borderBottomRightRadius: "20px",
+    borderTopRightRadius: "20px",
+    boxSizing: "border-box",
+  },
+  searchBtn: {
+    borderRadius: "20px",
+    fontSize: "0.7rem",
+    margin: "0",
+    boxShadow: "none",
+  },
+  searchIcon: {
+    color: "grey",
+  },
+  selectOptions: {
+    width: "80%",
+    margin: "auto",
+    borderTop: "2px solid #f4f4f4",
+    "&.Mui-selected": {
+      color: "#2524d6",
+      background: "white",
+      "&::after": {
+        content: '""',
+        width: "15px",
+        backgroundImage: "url(check.svg)",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+        fill: "#2524d6",
+        height: "15px",
+        position: "absolute",
+        right: "5px",
+        bottom: "10px",
+      },
+      "&:hover": {
         background: "white",
-        marginRight: "1px",
-        padding: "6px 30px 4px 30px",
-        borderBottomLeftRadius: "20px",
-        borderTopLeftRadius: "20px",
+      },
     },
-    searchSection: {
-        background: "white",
-        marginLeft: "1px",
-        padding: "4px 30px",
-        borderBottomRightRadius: "20px",
-        borderTopRightRadius: "20px",
-        boxSizing: "border-box",
+    "&:hover": {
+      color: "#2524d6",
+      background: "white",
     },
-    searchBtn: {
-        borderRadius: "20px",
-        fontSize: "0.7rem",
+    fontSize: "0.875rem",
+    padding: "5px 0",
+  },
+  selectType: {
+    fontSize: "0.875rem",
+    minWidth: "120px",
+    color: "#2524d6",
+    background: "white",
+  },
+  formControl: {
+    "& .MuiSvgIcon-root.MuiSelect-icon": {
+      left: "-25px",
+      right: "unset",
+      color: "#2524d6",
     },
-    searchIcon: {
-        position: "relative",
-        top: "4px",
-        right: "7px",
-        color: "grey",
+    "& .MuiSelect-select:focus": {
+      background: "white",
     },
-    searchInput: {
-        position: "relative",
-        top: "2px",
-        left: "4px",
-        fontSize: "0.875rem",
-    },
-    selectOptions: {
-        width: "80%",
-        margin: "auto",
-        borderTop: "2px solid #f4f4f4",
-        fontSize: "0.875rem",
-        wordWrap: "break-word",
-        paddingLeft: "0",
-    },
-    selectType: {
-        fontSize: "0.875rem",
-        width: "180px",
-        "& .Mui-selected": {
-            color: "red",
-        },
-    },
+  },
+  selectOptionsText: {
+    overflow: "hidden",
+    textOverflow: "ellipsis",
+    display: "inline",
+    margin: "0",
+    paddingRight: "25px",
+  },
 }));
 const SearchBar = (props: any) => {
-    const [type, setType] = useState(props.searchTypes[0].value);
-    const [searchString, setSearchString] = useState("");
-    const dispatch = useDispatch()
+  const [type, setType] = useState(props.searchTypes[0].value);
+  const [searchString, setSearchString] = useState("");
+  const dispatch = useDispatch();
 
-    const handleTypeChange = (e: any) => {
-        setType(e.target.value);
-    };
+  const handleTypeChange = (e: any) => {
+    setType(e.target.value);
+  };
 
-    const handleSubmit = async (e: any) => {
-        e.preventDefault();
-        if (props.role === "volunteers")
-            dispatch(searchVolunteers({ type, value: searchString }))
-        else
-            dispatch(searchAdmins({ type, value: searchString }))
-    };
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    if (props.role === "volunteers")
+      dispatch(searchVolunteers({ type, value: searchString }));
+    else dispatch(searchAdmins({ type, value: searchString }));
+  };
 
-    const handleChange = (e: any) => {
-        setSearchString(e.target.value);
-    };
+  const handleChange = (e: any) => {
+    setSearchString(e.target.value);
+  };
 
-    const classes = useStyles();
-    return (
-        <form onSubmit={handleSubmit}>
-            <Grid container className={classes.searchBar} justify="flex-end">
-                <Grid xs={4} item className={classes.selectSection}>
-                    <Select
-                        value={type}
-                        onChange={handleTypeChange}
-                        disableUnderline
-                        className={classes.selectType}
-                    >
-                        {props.searchTypes.map((type: any, index: number) => (
-                            <MenuItem
-                                key={type.value}
-                                value={type.value}
-                                className={classes.selectOptions}
-                                style={{
-                                    borderTop: index === 0 ? "none" : "2px solid #f4f4f4",
-                                }}
-                            >
-                                {type.demoValue}
-                                {/* <DoneIcon /> */}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                </Grid>
-                <Grid xs={7} item className={classes.searchSection}>
-                    <Grid container spacing={1} wrap="nowrap" justify="space-between">
-                        <Grid item container wrap="nowrap">
-                            <Grid item className={classes.searchIcon}>
-                                <SearchIcon />
-                            </Grid>
-                            <Grid item>
-                                <InputBase
-                                    placeholder="Search…"
-                                    className={classes.searchInput}
-                                    value={searchString}
-                                    onChange={handleChange}
-                                />
-                            </Grid>
-                        </Grid>
-                        <Grid item>
-                            <Button
-                                type="submit"
-                                variant="contained"
-                                color="primary"
-                                className={classes.searchBtn}
-                            >
-                                Search
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </form>
-    );
+  const classes = useStyles();
+  return (
+    <form onSubmit={handleSubmit}>
+      <Grid container justify="flex-end" wrap="nowrap">
+        <Grid item className={classes.selectSection}>
+          <FormControl className={classes.formControl}>
+            <Select
+              value={type}
+              onChange={handleTypeChange}
+              disableUnderline
+              className={classes.selectType}
+            >
+              {props.searchTypes.map((type: any, index: number) => (
+                <MenuItem
+                  key={type.value}
+                  value={type.value}
+                  className={classes.selectOptions}
+                  style={{
+                    borderTop: index === 0 ? "none" : "2px solid #f4f4f4",
+                  }}
+                >
+                  <p className={classes.selectOptionsText}>{type.demoValue}</p>
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </Grid>
+        <Grid item container className={classes.searchSection} wrap="nowrap">
+          <div>
+            <Input
+              value={searchString}
+              onChange={handleChange}
+              placeholder="Search"
+              disableUnderline
+              startAdornment={
+                <InputAdornment position="start">
+                  <SearchIcon className={classes.searchIcon} />
+                </InputAdornment>
+              }
+            />
+          </div>
+          <Button
+            type="submit"
+            variant="contained"
+            color="primary"
+            className={classes.searchBtn}
+          >
+            Search
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
 };
 
 export default SearchBar;
